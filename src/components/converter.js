09957c7c53b3e03a7d4ef0e6c6cbc507eff42699
Artofaj <info@artofakume.com>
2021-08-11 15:06:22 +0200

    Added conversion from roman to arabic

--- src/components/converter.js
+++ src/components/converter.js
@@ -1,57 +1,76 @@
 import React from "react"
 
 class RomanNumerals {
-
-    toRoman =() => "perro"
+  toRoman = () => "perro"
 }
 
 const Converter = () => {
+  const rules = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    XXX: 30,
+    XX: 20,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  }
 
-const toRoman = (num) => {
-    const rules = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      XXX: 30,
-      XX: 20,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1,
-    }
-
-    let res = ""
+  const toRoman = num => {
+    let result = ""
 
     const romans = Object.keys(rules)
-    
+
     for (let i = 0; i < romans.length; ++i) {
       const val = rules[romans[i]]
       while (num >= val) {
         num -= val
-        res += romans[i]
+        result += romans[i]
       }
     }
 
-    console.log(res)
-}
+    console.log(result)
+  }
 
-const fromRoman = () => "perro"
+  const fromRoman = string => {
+    let result = 0
+
+    let num = string.toUpperCase()
+
+    for (let i = 0; i + 1 < num.length; ++i) {
+      const current = rules[num.charAt(i)]
+      const next = rules[num.charAt(i + 1)]
+      if (current >= next) {
+        result += current
+      } else {
+        result -= current
+      }
+    }
+
+    console.log(result + rules[num.charAt(num.length - 1)])
+  }
 
   return (
     <form>
       <label htmlFor="arabic">
         Arabicae
-        <input type="number" id="arabic" placeholder={1000} onChange={e => toRoman(e.target.value)} />
+        <input
+          type="number"
+          id="arabic"
+          placeholder={1000}
+          onChange={e => toRoman(e.target.value)}
+        />
       </label>
       <label htmlFor="latinae">
         Latinae
-        <input type="text" id="latinae" placeholder="M"/>
+        <input type="text" id="latinae" placeholder="M" onChange={e => fromRoman(e.target.value) } />
       </label>
     </form>
   )
