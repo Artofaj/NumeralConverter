d746813036c241a6cb437b5c316a9820347f3917
Artofaj <info@artofakume.com>
2021-08-11 12:15:23 +0200

    Added basic component for catching input

---
+++ src/components/converter.js
@@ -0,0 +1,16 @@
+import React from "react"
+const Converter = () => {
+  return (
+    <form>
+      <label htmlFor="arabic">
+        Arabicae
+        <input inputType="number" id="arabic" />
+      </label>
+      <label htmlFor="latinae">
+        Latinae
+        <input inputType="number" id="latinae" />
+      </label>{" "}
+    </form>
+  )
+}
+export default Converter
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Converter from "../components/converter"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Converter" />
+    <Converter />
   </Layout>
 )
 
