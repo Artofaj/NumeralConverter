292a42a587ab9e4835ae971627c85e6b97b76f12
Artofaj <info@artofakume.com>
2021-08-12 13:43:46 +0200

    Reorganized files to make more sense

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `var(--clr-main)`,
+      background: `var(--clr-primary)`,
       marginBottom: `1.45rem`,
     }}
   >
